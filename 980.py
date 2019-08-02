0d9c7add33c3d5ad6c35469cbad7485dfe86383b
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-08-02 14:28:42 +0530

    Create 980.py

---
+++ 980.py
@@ -0,0 +1,10 @@
+for (l=0; l<k-1; l++) 
+            { 
+                if (temp[l].c == 0) 
+                { 
+                    temp[l].e = arr[i]; 
+                    temp[l].c = 1; 
+                    break; 
+                } 
+            } 
+  
