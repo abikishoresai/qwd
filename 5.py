bf703cefca420fdce2dba5aa5d97ac4bb8b53490
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-08-02 13:50:58 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+int main() 
+{ 
+    string str = "this is a test string"; 
+    string pat = "tist"; 
+  
+    cout << "Smallest window is : \n"
+        << findSubString(str, pat); 
+    return 0; 
+} 
