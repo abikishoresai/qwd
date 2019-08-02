49986e70dc0c21e57b36f1aff8d95a3cfe4669b9
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-08-02 14:43:32 +0530

    Create rty.py

---
+++ rty.py
@@ -0,0 +1,10 @@
+int main() 
+{ 
+    double l = 1; 
+    double h = 5; 
+    double w = 7; 
+    cout << "Area = " << areaCuboid(l, h, w) << endl; 
+    cout << "Total Surface Area = " 
+         << surfaceAreaCuboid(l, h, w); 
+    return 0; 
+} 
